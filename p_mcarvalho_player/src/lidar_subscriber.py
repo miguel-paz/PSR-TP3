f54234d36da12a389eecd4a74902162a6a7518d3
Pedro Amaral <pedromiglou@hotmail.com>
2022-02-20 18:33:09 +0000

    random wandering

--- p_mcarvalho_player/src/lidar_subscriber.py
+++ p_mcarvalho_player/src/lidar_subscriber.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 import math
+import random
 
 import rospy
-import std_msgs.msg
 from sensor_msgs.msg import LaserScan
-from sensor_msgs.msg import LaserScan, PointCloud2, PointField
-from sensor_msgs import point_cloud2
 from geometry_msgs.msg import Twist
 
 BURGER_MAX_LIN_VEL = 0.22
@@ -24,8 +22,9 @@ target_angular_vel  = 0.0
 control_linear_vel  = 0.0
 control_angular_vel = 0.0
 
-#publisher = rospy.Publisher('/left_laser/point_cloud', PointCloud2)
-pub = rospy.Publisher('/green1/green1/cmd_vel', Twist, queue_size=1)
+Turning = False
+
+pub = rospy.Publisher('/blue1/cmd_vel', Twist, queue_size=1)
 
 def vels(target_linear_vel, target_angular_vel):
     return "currently:\tlinear vel %s\t angular vel %s " % (target_linear_vel,target_angular_vel)
@@ -71,14 +70,9 @@ def checkAngularLimitVelocity(vel):
     return vel
 
 def callbackMessageReceived(msg):
-    global target_angular_vel, target_linear_vel, control_angular_vel, control_linear_vel
+    global target_angular_vel, target_linear_vel, control_angular_vel, control_linear_vel, Turning
     #rospy.loginfo('Received laser scan message')
     #rospy.loginfo(msg)
-
-    #header = std_msgs.msg.Header(seq=msg.header.seq, stamp=msg.header.stamp, frame_id=msg.header.frame_id)
-    #fields = [PointField('x', 0, PointField.FLOAT32, 1),
-    #          PointField('y', 4, PointField.FLOAT32, 1),
-    #          PointField('z', 8, PointField.FLOAT32, 1)]
 
     # convert from polar coordinates to cartesian and fill the point cloud
     points = []
@@ -90,22 +84,26 @@ def callbackMessageReceived(msg):
             y = range * math.sin(theta)
             points.append([x, y, z])
     
-    center_dist=4
-    if msg.ranges[0] < center_dist:
-        center_dist = msg.ranges[0]
+    center_dist=msg.range_max
+    for range in msg.ranges[0:int(math.pi/24/msg.angle_increment)]+msg.ranges[int(47*math.pi/24/msg.angle_increment):]:
+        if range >= msg.range_min and range <= msg.range_max and range < center_dist:
+            center_dist = range
     
-    right_dist=4
-    if msg.ranges[int(1.5*math.pi/msg.angle_increment)] < right_dist:
-        right_dist = msg.ranges[int(1.5*math.pi/msg.angle_increment)]
+    right_dist=msg.range_max
+    for range in msg.ranges[int(1.5*math.pi/msg.angle_increment):int(11*math.pi/6/msg.angle_increment)]:
+        if range >= msg.range_min and range <= msg.range_max and range < right_dist:
+            right_dist = range
     
-    left_dist=4
-    if msg.ranges[int(1.5*math.pi/msg.angle_increment)] < left_dist:
-        left_dist = msg.ranges[int(0.5*math.pi/msg.angle_increment)]
+    left_dist=msg.range_max
+    for range in msg.ranges[int(math.pi/6/msg.angle_increment):int(0.5*math.pi/msg.angle_increment)]:
+        if range >= msg.range_min and range <= msg.range_max and range < left_dist:
+            left_dist = range
     
-    if center_dist>3.5 and left_dist>1.5 and right_dist>1.5:
+    if center_dist>2.5 and left_dist>0.3 and right_dist>0.3:
+        Turning = False
         twist = Twist()
 
-        control_linear_vel = makeSimpleProfile(control_linear_vel, center_dist/5, (LIN_VEL_STEP_SIZE/2.0))
+        control_linear_vel = makeSimpleProfile(control_linear_vel, 0.7, (LIN_VEL_STEP_SIZE/2.0))
         twist.linear.x = control_linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
 
         control_angular_vel = makeSimpleProfile(control_angular_vel, 0, (ANG_VEL_STEP_SIZE/2.0))
@@ -114,33 +112,36 @@ def callbackMessageReceived(msg):
         pub.publish(twist)
 
     else:
+        
         twist = Twist()
 
-        control_linear_vel = makeSimpleProfile(control_linear_vel, 0, (LIN_VEL_STEP_SIZE/2.0))
+        control_linear_vel = makeSimpleProfile(control_linear_vel, 0.2, (LIN_VEL_STEP_SIZE/2.0))
         twist.linear.x = control_linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
-
-        control_angular_vel = makeSimpleProfile(control_angular_vel, 1.5 if left_dist>right_dist else -1.5, (ANG_VEL_STEP_SIZE/2.0))
+        
+        if left_dist<2 or right_dist<2:
+            if left_dist>right_dist:
+                Turning=0.5
+            else:
+                Turning=-0.5
+        
+        elif not Turning:
+            Turning = 0.5 if random.random()>0.5 else -0.5
+            
+        control_angular_vel = makeSimpleProfile(control_angular_vel, Turning, (ANG_VEL_STEP_SIZE/2.0))
         twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = control_angular_vel
 
         pub.publish(twist)
-        
-
-    #pc2 = point_cloud2.create_cloud(header, fields, points)  # create point_cloud2 data structure
-    #publisher.publish(pc2)  # publish (will automatically convert from point_cloud2 to Pointcloud2 message)
-    #rospy.loginfo('Published PointCloud2 msg')
 
 
 def main():
 
-    rospy.init_node('lidar_subscriber_green', anonymous=False)
+    rospy.init_node('lidar_subscriber_blue', anonymous=False)
 
-    rospy.Subscriber('/green1/green1/scan', LaserScan, callbackMessageReceived)
+    rospy.Subscriber('/blue1/scan', LaserScan, callbackMessageReceived)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 
 if __name__ == '__main__':
-
-    
     main()
