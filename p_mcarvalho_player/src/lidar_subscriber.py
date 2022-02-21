1452b9dfeed3fa8794e59846c49310fcd44f5a6b
Pedro Amaral <pedromiglou@hotmail.com>
2022-02-21 22:47:25 +0000

    obtain other robots coordinates using lidar

--- p_mcarvalho_player/src/lidar_subscriber.py
+++ p_mcarvalho_player/src/lidar_subscriber.py
@@ -6,6 +6,9 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
+from sklearn.cluster import DBSCAN
+import numpy as np
+
 BURGER_MAX_LIN_VEL = 0.22
 BURGER_MAX_ANG_VEL = 2.84
 
@@ -77,46 +80,55 @@ def callbackMessageReceived(msg):
     # convert from polar coordinates to cartesian and fill the point cloud
     points = []
     z = 0
-    for idx, range in enumerate(msg.ranges):
-        if range >= msg.range_min and range <= msg.range_max:
+    for idx, r in enumerate(msg.ranges):
+        if r >= msg.range_min and r <= msg.range_max:
             theta = msg.angle_min + msg.angle_increment * idx
-            x = range * math.cos(theta)
-            y = range * math.sin(theta)
+            x = r * math.cos(theta)
+            y = r * math.sin(theta)
             points.append([x, y, z])
     
+    points = np.array(points)
+    
+    clustering = DBSCAN(eps=0.5, min_samples=1).fit(points)
+    
+    #print(clustering.labels_)
+    
+    robot_labels = []
+    for label in set(clustering.labels_):
+        if 2<= len([l for l in clustering.labels_ if l==label]) <=20:
+            robot_labels.append(label)
+    
+    robots = [] # coordinates of possible robots 
+    for rl in robot_labels:
+        robot_points = [points[i,:] for i in range(len(clustering.labels_)) if clustering.labels_[i]==rl]
+        x = sum([p[0] for p in robot_points])/len(robot_points)
+        y = sum([p[1] for p in robot_points])/len(robot_points)
+        z = sum([p[2] for p in robot_points])/len(robot_points)
+        robots.append([x, y, z])
+
     center_dist=msg.range_max
-    for range in msg.ranges[0:int(math.pi/24/msg.angle_increment)]+msg.ranges[int(47*math.pi/24/msg.angle_increment):]:
-        if range >= msg.range_min and range <= msg.range_max and range < center_dist:
-            center_dist = range
+    for r in msg.ranges[0:int(math.pi/24/msg.angle_increment)]+msg.ranges[int(47*math.pi/24/msg.angle_increment):]:
+        if r >= msg.range_min and r <= msg.range_max and r < center_dist:
+            center_dist = r
     
     right_dist=msg.range_max
-    for range in msg.ranges[int(1.5*math.pi/msg.angle_increment):int(11*math.pi/6/msg.angle_increment)]:
-        if range >= msg.range_min and range <= msg.range_max and range < right_dist:
-            right_dist = range
+    for r in msg.ranges[int(1.5*math.pi/msg.angle_increment):int(11*math.pi/6/msg.angle_increment)]:
+        if r >= msg.range_min and r <= msg.range_max and r < right_dist:
+            right_dist = r
     
     left_dist=msg.range_max
-    for range in msg.ranges[int(math.pi/6/msg.angle_increment):int(0.5*math.pi/msg.angle_increment)]:
-        if range >= msg.range_min and range <= msg.range_max and range < left_dist:
-            left_dist = range
+    for r in msg.ranges[int(math.pi/6/msg.angle_increment):int(0.5*math.pi/msg.angle_increment)]:
+        if r >= msg.range_min and r <= msg.range_max and r < left_dist:
+            left_dist = r
     
     if center_dist>2.5 and left_dist>0.3 and right_dist>0.3:
         Turning = False
-        twist = Twist()
-
+        
         control_linear_vel = makeSimpleProfile(control_linear_vel, 0.7, (LIN_VEL_STEP_SIZE/2.0))
-        twist.linear.x = control_linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
-
         control_angular_vel = makeSimpleProfile(control_angular_vel, 0, (ANG_VEL_STEP_SIZE/2.0))
-        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = control_angular_vel
-
-        pub.publish(twist)
 
     else:
-        
-        twist = Twist()
-
         control_linear_vel = makeSimpleProfile(control_linear_vel, 0.2, (LIN_VEL_STEP_SIZE/2.0))
-        twist.linear.x = control_linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
         
         if left_dist<2 or right_dist<2:
             if left_dist>right_dist:
@@ -128,9 +140,11 @@ def callbackMessageReceived(msg):
             Turning = 0.5 if random.random()>0.5 else -0.5
             
         control_angular_vel = makeSimpleProfile(control_angular_vel, Turning, (ANG_VEL_STEP_SIZE/2.0))
-        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = control_angular_vel
 
-        pub.publish(twist)
+    twist = Twist()
+    twist.linear.x = control_linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
+    twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = control_angular_vel
+    pub.publish(twist)
 
 
 def main():
