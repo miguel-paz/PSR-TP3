dac71e7e0adb137ab5920c98def755f71baa2f4c
Paz <miguel.paz.carvalho@ua.pt>
2022-03-04 05:46:30 +0000

    Testing connection from coach

--- p_mcarvalho_bringup/src/coach.py
+++ p_mcarvalho_bringup/src/coach.py
@@ -4,6 +4,7 @@ from std_msgs.msg import String
 from p_mcarvalho_bringup.msg import Coach_msg
 import cv2
 from cv_bridge import CvBridge
+import sqlite3
 
 class Coach:
 
@@ -19,12 +20,35 @@ class Coach:
 
         self.bridge = CvBridge()
 
+        # try:
+        #     sqliteConnection = sqlite3.connect('TP3_Django/psr_tp3_test/db.sqlite3')
+        #     cursor = sqliteConnection.cursor()
+        #     print("Successfully Connected to Django DB")
+
+        #     cursor.execute('''INSERT INTO Coach(name) VALUES (self.name)''')
+            
+        #     conn.commit()
+        #     cursor.close()
+
+        # except sqlite3.Error as error:
+        #     print("Error while connecting to Django DB", error)
+        # finally:
+        #     if sqliteConnection:
+        #         sqliteConnection.close()
+        #         print("The Django DB connection is closed")
+
         topic = self.name + "_comm"
         print('Subscribing to topic ' + topic)
         rospy.Subscriber(topic, Coach_msg, self.callbackmsg)
 
     def callbackmsg(self, data):
         content = data.content.split('/')
+        data_previous_state = content[0]
+        data_current_state = content[1]
+        data_reason = content[2]
+        new_rob = False
+        valid_data = False
+
         
         # if data_type=='Init':
         #     if self.team_color in data_sender:
@@ -38,16 +62,36 @@ class Coach:
                 return
             if data.sender not in self.team:
                 print('Robot ' + data.sender + ' added to team of coach ' + self.name)
-            data_previous_state = content[0]
-            data_current_state = content[1]
-            data_reason = content[2]
+                new_rob = True
             self.team[data.sender] = {'previous_state': data_previous_state, 'current_state': data_current_state, 'reason': data_reason} #, 'front_camera': data.front_camera 'back_camera': data.back_camera}
             print('Robot ' + data.sender + ' is changing from state ' + data_previous_state + ' to state ' + data_current_state + ' because it ' + data_reason)
+            valid_data = True
         else:
             print('Unrecognized message type by robot ' + data.sender)
             return
 
         print(self.team)
+
+        # if valid_data:
+        #     try:
+        #         sqliteConnection = sqlite3.connect('TP3_Django/psr_tp3_test/db.sqlite3')
+        #         cursor = sqliteConnection.cursor()
+        #         print("Successfully Connected to Django DB")
+
+        #         if new_rob:
+        #             cursor.execute('''INSERT INTO Robot(name,state,coach) VALUES (data.sender,data_current_state,self.name)''')
+                
+        #         cursor.execute('''INSERT INTO Message(sender,previous_state,current_state,reason,receiver) VALUES (data.sender)''')
+        #         conn.commit()
+        #         cursor.close()
+
+        #     except sqlite3.Error as error:
+        #         print("Error while connecting to Django DB", error)
+        #     finally:
+        #         if sqliteConnection:
+        #             sqliteConnection.close()
+        #             print("The Django DB connection is closed")
+
 
         try:
             cv_image_front = self.bridge.imgmsg_to_cv2(data.front_camera, "bgr8")
@@ -59,7 +103,6 @@ class Coach:
         cv2.imshow(data.sender + ' Front Camera', cv_image_front)
         cv2.imshow(data.sender + ' Back Camera', cv_image_back)
         #cv2.imwrite('testcoachcam.jpg',cv_image_front)
-
         cv2.waitKey(3)
         
 
